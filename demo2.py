3a499bbc2c3727dd62d62c8abc00c0b431049183
陈景欣 <jingxinchen281@gmail.com>
2023-05-14 18:31:59 +0800

    Update demo2.py

--- demo2.py
+++ demo2.py
@@ -5,18 +5,10 @@ from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain.prompts import PromptTemplate
 import io
 
-from yaml import DocumentEndEvent
-
-st.sidebar.title("一键文档优化工具")
+st.title("一键文档优化工具")
 
 llm = OpenAI(temperature=0.7, max_tokens=2500)
 chains = []
-# Add a radio button for navigation
-st.sidebar.markdown("<h2>导航</h2>", unsafe_allow_html=True)
-page = st.sidebar.radio("", ["优化文本", "优化 Word 文件"])
-
-
-st.sidebar.header("优化功能选择：")
 
 # 为每一个优化功能创建一个复选框，并根据用户的选择来创建对应的模型链
 if st.sidebar.checkbox("拼写检查优化"):
@@ -51,18 +43,19 @@ if st.sidebar.checkbox("段落结构优化"):
 
 sequential_chain = SimpleSequentialChain(chains=chains)
 
-
-
+# Add a radio button for navigation
+st.sidebar.markdown("<h3>导航</h3>", unsafe_allow_html=True)
+page = st.sidebar.radio("", ["优化文本", "优化 Word 文件"])
 
 if page == "优化文本":
     # Add a text area for user to paste the text
-    input_text = st.text_area("请在此处粘贴文本进行优化：", "", height=400)
+    input_text = st.text_area("在此处粘贴文本进行优化", "", height=300)
 
     # Create two columns for displaying the original and optimized text side by side
     col1, col2 = st.columns(2)
 
     # Add a button to trigger the processing
-    if st.button('开始优化文本'):
+    if st.button('优化文本'):
         if input_text:
             response = sequential_chain.run(input_text)
             col1.write("原始文本：")
@@ -70,13 +63,14 @@ if page == "优化文本":
             col2.write("优化后的文本：")
             col2.markdown(response)
 
-elif page == "优化 Word 文件":
+else:
     # Add a file uploader for user to upload a docx file
     uploaded_file = st.file_uploader("上传一个 Word 文件", type=["docx"])
 
     # If there is any uploaded file, process it
     if uploaded_file is not None:
         document = docx.Document(uploaded_file)
+
         # Read the document and concatenate all the paragraphs into a single string
         document_text = "\n".join([para.text for para in document.paragraphs])
 
@@ -92,11 +86,15 @@ elif page == "优化 Word 文件":
         new_document.save(new_docx)
         new_docx.seek(0)
 
-        # Download the optimized document
-        if st.button('下载优化后的 Word 文件'):
+        # Create an empty element for the download button
+        download_button_placeholder = st.empty()
+
+        # Update the placeholder with the actual download button
+        if download_button_placeholder.button('下载优化后的 Word 文件'):
             st.download_button(
                 label="点击下载",
                 data=new_docx,
                 file_name='optimized_document.docx',
                 mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document'
             )
+
